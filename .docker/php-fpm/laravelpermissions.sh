78e5abfc51102ef647b7c8623eff7f9553d0ce1f
AndersonPEM <andersonsilva.souza@outlook.com>
2020-11-23 15:51:06 -0300

    UX

--- .docker/php-fpm/laravelpermissions.sh
+++ .docker/php-fpm/laravelpermissions.sh
@@ -1,8 +1,12 @@
-!#/bin/bash
+#!/usr/bin/env bash
 path=/var/www/html
 cd $path
-sudo chown -R $APACHE_USER:$APACHE_GROUP .
+echo "Changing owner to $APACHE_RUN_USER:$APACHE_RUN_GROUP"
+sudo chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP .
+echo "Changing permissions for files and folders"
 sudo find $path -type f -exec chmod 644 {} \;
 sudo find $path -type d -exec chmod 755 {} \;
-sudo chgrp -R $APACHE_GROUP storage bootstrap/cache
+echo "Changing permissions for write-enabled directories..."
+sudo chgrp -R $APACHE_RUN_GROUP storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
+echo "You're good to go."
